f5176742c558765ec020fdf5cbbb066d5751f972
oysteinwaage <oysteinwaage@hotmail.com>
2026-05-26 23:33:41 +0200

    hoster nå også kampoppsettet til miniligaen

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,24 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
+const staticPages = ['/kampoppsett-obos-miniliga-roa'];
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: 'serve-static-pages',
+      configureServer(server) {
+        server.middlewares.use((req, _res, next) => {
+          if (req.url && staticPages.some(p => req.url === p || req.url === p + '/')) {
+            req.url = req.url.replace(/\/?$/, '/index.html');
+          }
+          next();
+        });
+      },
+    },
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
